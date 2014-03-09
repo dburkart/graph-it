34b9b58bf906cf8e39d98a91d9aa1c04252842c3
Dana Burkart <dana.burkart@gmail.com>
2014-03-09 10:01:45 -0700

    Rough outline of the main web-page.

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express')
+  , path = require('path')
+  , http = require('http');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 9000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  //app.use(express.session({ secret: 'tobo!', store: store }));
+  app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.locals.pretty = true;
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+	res.render('index', {
+		title: 'Graph-It'
+	});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
